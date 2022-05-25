4bee2769264c5cef26393ece83cdc27b364c6499
zoe-phoria <mueller-zoe@pm.me>
2022-05-25 23:44:54 +0200

    new script to make symlinks in home xdg user dirs

---
+++ xdg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xdg-user-dirs-update --set DOCUMENTS /home/zoe/Documents/
+xdg-user-dirs-update --set PICTURES /home/zoe/Pictures/
+xdg-user-dirs-update --set MUSIC /home/zoe/Music/
+xdg-user-dirs-update --set VIDEOS /home/zoe/Videos/
