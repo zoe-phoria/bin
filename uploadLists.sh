0fba74dc4ee8db2da561e241df689fbdc2565dfc
zoe-phoria <mueller-zoe@pm.me>
2022-10-15 09:50:05 +0200

    new script pacint, fix colors in uploadLists.sh

--- uploadLists.sh
+++ uploadLists.sh
@@ -6,8 +6,8 @@ then
     /usr/bin/pacman -Qqetn > /mnt/files/Software/pacman/nodependpkglist.txt 
     /usr/bin/pacman -Qqem > /mnt/files/Software/pacman/foreignpkglist.txt
     /usr/bin/flatpak list > /mnt/files/Software/pacman/flatpakpkglist.txt
-    printf "\033[0;34m::\033[0m package lists updated\n"
+    printf "\033[01;34m::\033[0m package lists updated\n"
 else
-    printf "\033[0;34m::\033[0m host 'athena' (192.168.0.3) is down\n"
-    printf "\033[0;34m::\033[0m package lists not updated\n"
+    printf "\033[01;34m::\033[0m host 'athena' (192.168.0.3) is down\n"
+    printf "\033[01;34m::\033[0m package lists not updated\n"
 fi
