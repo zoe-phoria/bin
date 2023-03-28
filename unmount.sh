1130577ab9cacacf46f9a39ec956e62c1533577d
zoe-phoria <mueller-zoe@pm.me>
2023-03-28 21:51:39 +0200

    make cmds verbose and stuff

--- unmount.sh
+++ unmount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo umount --lazy /mnt/files
-sudo umount --lazy /mnt/music
-sudo umount --lazy /mnt/mpic
+sudo /sbin/umount -v --lazy /mnt/files
+sudo /sbin/umount -v --lazy /mnt/music
