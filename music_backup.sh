9662ccd37620104d1bb4cec0503cf368b02677ad
zoe-phoria <mueller-zoe@pm.me>
2023-03-29 12:38:10 +0200

    make more readable, save log

--- music_backup.sh
+++ music_backup.sh
@@ -1,1 +1,8 @@
-rsync -avb --progress --delete-after --backup-dir=/mnt/backup_music/rsync_backup/ /mnt/music /mnt/backup_music >> Downloads/rsync_log
+#!/bin/bash
+
+SOURCE=/mnt/music
+TARGET=/mnt/backup_music
+BACKUP=/mnt/backup_music
+
+# archive mode, verbose, backup mode, show progress, delete after completion, exclude #recycle
+rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' $SOURCE $TARGET >> "/mnt/backup_music/rsync_log_$(date)"
