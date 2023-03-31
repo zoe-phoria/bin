435a7fc8347e7e86e3a5e95e61ecbca576255590
zoe-phoria <mueller-zoe@pm.me>
2023-03-31 20:05:06 +0200

    add bandwidth limit

--- music_backup.sh
+++ music_backup.sh
@@ -3,6 +3,7 @@
 SOURCE=/mnt/music
 TARGET=/mnt/backup_music
 BACKUP=/mnt/backup_music
+BANDWIDTH=500 #kBps
 
 # archive mode, verbose, backup mode, show progress, delete after completion, exclude #recycle
-rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' $SOURCE $TARGET >> "/mnt/backup_music/rsync_log_$(date)"
+rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/backup_music/rsync_log_$(date)"
