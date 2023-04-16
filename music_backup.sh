704ae8b89edfc47a63e940f8e819389edaccd35e
zoe-phoria <mueller-zoe@pm.me>
2023-04-16 21:19:57 +0200

    increase bandwidth (faster wifi yay)

--- music_backup.sh
+++ music_backup.sh
@@ -3,7 +3,7 @@
 SOURCE=/mnt/music
 TARGET=/mnt/backup_music
 BACKUP=/mnt/backup_music
-BANDWIDTH=500 #kBps
+BANDWIDTH=10000 #kBps
 
 # archive mode, verbose, backup mode, show progress, delete after completion, exclude #recycle
 rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/backup_music/rsync_log_$(date)"
