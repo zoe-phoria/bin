1f6e5f2f7a529c4787b64c6c1cbd3f993e3c1726
zoe-phoria <mueller-zoe@pm.me>
2023-05-08 12:41:23 +0200

    renamed mount point

--- music_backup.sh
+++ music_backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SOURCE=/mnt/music
-TARGET=/mnt/backup_music
-BACKUP=/mnt/backup_music
+TARGET=/mnt/music_backup
+BACKUP=/mnt/music_backup
 BANDWIDTH=10000 #kBps
 
 # archive mode, verbose, backup mode, show progress, delete after completion, exclude #recycle
-rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/backup_music/rsync_log_$(date)"
+rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/music_backup/rsync_log_$(date)"
