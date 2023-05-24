459f43e5e98236c2d04f8d5702ad9e8f58195381
zoe-phoria <mueller-zoe@pm.me>
2023-05-24 12:38:32 +0200

    change hostname of nas

--- uploadLists.sh
+++ uploadLists.sh
@@ -8,6 +8,6 @@ then
     /usr/bin/flatpak list > /mnt/files/Software/pacman/flatpakpkglist.txt
     printf "\033[01;34m::\033[0m package lists updated\n"
 else
-    printf "\033[01;34m::\033[0m host 'athena' (192.168.0.3) is down\n"
+    printf "\033[01;34m::\033[0m host 'ds718p' (192.168.0.3) is down\n"
     printf "\033[01;34m::\033[0m package lists not updated\n"
 fi
