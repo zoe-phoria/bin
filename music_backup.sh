7417cf3447157377b688942f888fc0cebec0146b
zoe-phoria <mueller-zoe@pm.me>
2023-07-13 17:55:01 +0200

    make backup task immune to SIGINT etc and show output

--- music_backup.sh
+++ music_backup.sh
@@ -6,4 +6,7 @@ BACKUP=/mnt/music_backup
 BANDWIDTH=10000 #kBps
 
 # archive mode, verbose, backup mode, show progress, delete after completion, exclude #recycle
-rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/music_backup/rsync_log_$(date)"
+# run in [(subshell)] and create new process grounp [set -m] to make it immune to SIGINT etc
+(set -m; nohup rsync -avb --progress --delete-after --backup-dir=$BACKUP --exclude '#recycle' --bwlimit=$BANDWIDTH $SOURCE $TARGET >> "/mnt/music_backup/rsync_log_$(date)" & )
+# show output
+tail +1f "/mnt/music_backup/rsync_log_$(date)"
