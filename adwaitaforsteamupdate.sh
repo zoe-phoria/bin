403cfe659bf7aa64170a2ae0cd0552535c5efba6
zoe-phoria <mueller-zoe@pm.me>
2023-10-26 00:01:51 +0200

    move the adwaitaforsteam clone out of the way

--- adwaitaforsteamupdate.sh
+++ adwaitaforsteamupdate.sh
@@ -1,4 +1,4 @@
-cd ~/Downloads/Adwaita-for-Steam/
+cd ~/.steam/Adwaita-for-Steam/
 git pull
-chmod +x ~/Downloads/Adwaita-for-Steam/install.py
+chmod +x ~/.steam/Adwaita-for-Steam/install.py
 ./install .py -e general/no_rounded_window_corners -e library/hide_whats_new
