fc785ff911d6b82a910d0696718841bbb581a8ec
zoe-phoria <mueller-zoe@pm.me>
2023-10-29 22:01:29 +0100

    add shebang, move the repo around again and call explicitly with python3

--- adwaitaforsteamupdate.sh
+++ adwaitaforsteamupdate.sh
@@ -1,4 +1,6 @@
-cd ~/.steam/Adwaita-for-Steam/
+#!/bin/bash
+
+cd $HOME/builds/Adwaita-for-Steam/
 git pull
-chmod +x ~/.steam/Adwaita-for-Steam/install.py
-./install .py -e general/no_rounded_window_corners -e library/hide_whats_new
+chmod +x $HOME/builds/Adwaita-for-Steam/install.py
+/usr/bin/python3 $HOME/builds/Adwaita-for-Steam/install.py -e general/no_rounded_window_corners -e library/hide_whats_new
