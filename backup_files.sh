83eda8cd218bf67bf385e2ec9248857d86f69304
zoe-phoria <mueller-zoe@pm.me>
2024-01-27 22:17:37 +0100

    new backup scripts yay (run on file server just for archival purposes)

---
+++ backup_files.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# check if output is a terminal
+if [ -t 1 ]; then
+    printf "please run this script with nohup\n"
+    exit 1
+fi
+
+SOURCE=/volume1/files
+TARGET=/volumeUSB1/usbshare
+DISKLABEL=$(cat /etc/mtab | grep usbshare | awk '{print $1}')
+SIZE=$(du -sL /volume1/files | awk '{print $1}')
+AVAIL=$(df $DISKLABEL --output=avail | tail -n 1)
+
+printf "copy size:      $SIZE\n"
+printf "available size: $AVAIL\n"
+
+if [[ -f $TARGET/files.dummy ]] && (( $SIZE < $AVAIL )); then
+    printf "creating backup $(date +"%Y-%m-%d_%H-%M")\n"
+    rsync -av $SOURCE ${TARGET}/files_$(date +"%Y-%m-%d_%H-%M") >> "$TARGET/rsync_log_$(date +"%Y-%m-%d_%H-%M")"
+elif [[ -f $TARGET/files.dummy ]]; then
+    printf "not enough space\n"
+else
+    printf "files.dummy missing (wrong drive attached)\n"
+fi
---
+++ backup_music.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# check if output is a terminal
+if [ -t 1 ]; then
+    printf "please run this script with nohup (nohup ./backup_music.sh &)\n"
+    exit 1;
+fi
+
+SOURCE=/volume1/music
+TARGET=/volumeUSB1/usbshare
+
+rsync -avb --delete-after --backup-dir=${TARGET}/rsync_backup --exclude '#recycle' $SOURCE ${TARGET}/music/ >> "${TARGET}/rsync_log_$(date +"%Y-%m-%d_%H-%M")"
