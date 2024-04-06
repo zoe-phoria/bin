8a7c6d82d023c08c8a94a05b1c1a39c3ffdfa86a
zoe-phoria <mueller-zoe@pm.me>
2024-04-06 18:39:39 +0200

    remove obsolete mac only scripts

--- syncDocs_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/bash
-
-if [[ -f /Volumes/files/passwords.kdbx ]]; then
-    rsync -a -E -v --delete -b --backup-dir=/Users/zoe/DocumentsDeleted/ -P --stats /Volumes/files/Documents/ /Users/zoe/DocumentsBackup/
-    rm /Users/zoe/.sync.err
-else
-    printf "$(date +"%Y-%m-%d_%H-%M"): ds718p not connected; syncDocs unsuccessful\n" >> /Users/zoe/.sync.err
-fi
--- syncPass_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/bash
-
-if [[ -f /Volumes/files/passwords.kdbx ]]; then
-    cp -v /Volumes/files/passwords.kdbx /Users/zoe/passwords.kdbx
-    rm /Users/zoe/.sync.err
-else
-    printf "$(date +"%Y-%m-%d_%H-%M"): ds718p not connected; syncPass unsuccessful\n" >> /Users/zoe/.sync.err
-fi
