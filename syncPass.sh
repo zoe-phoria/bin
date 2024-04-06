3b5588acb18bf7f72a551cd86b79a82792f6a055
zoe-phoria <mueller-zoe@pm.me>
2024-04-06 19:14:28 +0200

    add $HOME to relevant paths

--- syncPass.sh
+++ syncPass.sh
@@ -11,8 +11,8 @@ else
 fi
 
 if [[ -f $SOURCE ]]; then
-    date +"%Y-%m-%d_%H-%M" >> .syncPass.log
-    cp -v  $SOURCE $DEST >> .syncPass.log
+    date +"%Y-%m-%d_%H-%M" >> $HOME/.syncPass.log
+    cp -v  $SOURCE $DEST >> $HOME/.syncPass.log
     rm $HOME/.sync.err &>/dev/null
 else
     printf "$(date +"%Y-%m-%d_%H-%M"): database file not found; syncPass unsuccessful; is ds718p connected?\n" >> $HOME/.sync.err
