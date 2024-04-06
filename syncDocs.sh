3b437cc4e5bb34187a140867d34dbfb1a6a31883
zoe-phoria <mueller-zoe@pm.me>
2024-04-06 18:35:44 +0200

    unify mac and linux document folder and passwords database syncing

--- syncDocs.sh
+++ syncDocs.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-rsync -a -E -v --delete -b --backup-dir=/mnt/SSD1000/DocumentsDeleted/ -P --stats /mnt/files/Documents/ /mnt/SSD1000/DocumentsBackup/
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    SOURCE=/mnt/files/Documents/
+    DEST=/mnt/SSD1000/DocumentsBackup/
+    DEL=/mnt/SSD1000/DocumentsDeleted/
+elif [[ $OSTYPE == "darwin"* ]]; then
+    SOURCE=/Volumes/files/Documents/
+    DEST=/Users/zoe/DocumentsBackup/
+    DEL=/Users/zoe/DocumentsDeleted/
+else
+    exit 1
+fi
+
+if [[ -f $SOURCE/../passwords.kdbx ]]; then
+    rsync -a -E -v --delete -b --backup-dir=$DEL -P --stats $SOURCE $DEST
+    rm $HOME/.sync.err > /dev/null
+else
+    printf "($date +"%Y-%m-%d_%H-%M"): passwords.kdbx not found; syncDocs unsuccessful; is ds718p connected?\n" >> $HOME/.sync.err
+fi
--- syncPass.sh
+++ syncPass.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-cp -u --backup -v /mnt/files/passwords.kdbx /mnt/SSD1000/passwords.kdbx 
+if [[ $OSTYPE == "linux-gnu" ]]; then
+    SOURCE=/mnt/files/passwords.kdbx
+    DEST=/mnt/SSD1000/passwords.kdbx
+elif [[ $OSTYPE == "darwin"* ]]; then
+    SOURCE=/Volumes/files/passwords.kdbx
+    DEST=/Users/zoe/passwords.kdbx
+else
+    exit 1
+fi
+
+if [[ -f $SOURCE/../passwords.kdbx ]]; then
+    cp -u --backup -v $SOURCE $DEST
+    rm $HOME/.sync.err > /dev/null
+else
+    printf "($date +"%Y-%m-%d_%H-%M"): database file not found; syncPass unsuccessful; is ds718p connected?\n" >> $HOME/.sync.err
+fi
