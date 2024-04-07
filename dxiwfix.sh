e29df76ff5a9ae6b66cd9e2c6939ff625d17e174
zoe-phoria <mueller-zoe@pm.me>
2024-04-07 12:47:09 +0200

    delete dxiwfix script

--- dxiwfix.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp /mnt/SSD500/SteamLibrary/steamapps/compatdata/6920/pfx/drive_c/users/steamuser/My\ Documents/Deus\ Ex\ -\ Invisible\ War/userres.ini /mnt/SSD500/SteamLibrary/steamapps/compatdata/6920/pfx/drive_c/users/steamuser/My\ Documents/Deus\ Ex\ -\ Invisible\ War/user.ini
