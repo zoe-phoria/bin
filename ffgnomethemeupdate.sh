4149fe68c9c9ceec99c09f6ff14f0743061e57c6
zoe-phoria <mueller-zoe@pm.me>
2024-04-10 11:47:52 +0200

    make script macos compatible

--- ffgnomethemeupdate.sh
+++ ffgnomethemeupdate.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-echo
-echo "check https://github.com/rafaelmardojai/firefox-gnome-theme before proceeding"
+printf "\ncheck https://github.com/rafaelmardojai/firefox-gnome-theme before proceeding\n"
 read -p "proceed? (y/N) " -n 1 -r
-echo
-echo
+printf "\n\n"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  PROFILE=$(ls -l $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')
-  cd /home/zoe/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/
+  if [[ $OSTYPE == "linux-gnu" ]]; then
+    PROFILE=$(ls -l $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')
+    cd /home/zoe/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/
+  elif [[ $OSTYPE == "darwin"* ]]; then
+    if [[ -z $(ls $HOME/appsupport) ]]; then
+      ln -sf $HOME/Library/Application Support/ $HOME/appsupport
+    fi
+    PROFILE=$(ls -l /Users/zoe/appsupport/Firefox/Profiles/ | grep default-release | awk '{print $NF}')
+    cd /Users/zoe/Library/Application\ Support/Firefox/Profiles/$PROFILE/chrome
+  else
+    printf "unsupported OS\n"
+  fi
   git pull
 fi
-echo
+printf "\n"
