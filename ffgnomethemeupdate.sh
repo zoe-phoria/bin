5054790069e4a0ae068a78039e8b597ac1c18971
zoe-phoria <mueller-zoe@pm.me>
2024-04-10 13:47:44 +0200

    change tabs to make more readable

--- ffgnomethemeupdate.sh
+++ ffgnomethemeupdate.sh
@@ -5,18 +5,18 @@ read -p "proceed? (y/N) " -n 1 -r
 printf "\n\n"
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  if [[ $OSTYPE == "linux-gnu" ]]; then
-    PROFILE=$(ls -l $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')
-    cd /home/zoe/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/
-  elif [[ $OSTYPE == "darwin"* ]]; then
-    if [[ -z $(ls $HOME/appsupport) ]]; then
-      ln -sf $HOME/Library/Application Support/ $HOME/appsupport
+    if [[ $OSTYPE == "linux-gnu" ]]; then
+        PROFILE=$(ls -l $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')
+        cd /home/zoe/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/
+    elif [[ $OSTYPE == "darwin"* ]]; then
+        if [[ -z $(ls $HOME/appsupport) ]]; then
+            ln -sf $HOME/Library/Application Support/ $HOME/appsupport
+        fi
+        PROFILE=$(ls -l $HOME/appsupport/Firefox/Profiles/ | grep default-release | awk '{print $NF}')
+        cd $HOME/Library/Application\ Support/Firefox/Profiles/$PROFILE/chrome
+    else
+        printf "unsupported OS\n"
     fi
-    PROFILE=$(ls -l $HOME/appsupport/Firefox/Profiles/ | grep default-release | awk '{print $NF}')
-    cd $HOME/Library/Application\ Support/Firefox/Profiles/$PROFILE/chrome
-  else
-    printf "unsupported OS\n"
-  fi
-  git pull
+    git pull
 fi
 printf "\n"
