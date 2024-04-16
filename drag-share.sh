d7d83a558bb29f1fcd13554b9bc51a03f8267c10
zoe-phoria <mueller-zoe@pm.me>
2024-04-16 21:13:07 +0200

    use dynamic home

--- drag-share.sh
+++ drag-share.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-file=$(/home/zoe/bin/dragon --target --print-path --and-exit)
+file=$($HOME/bin/dragon --target --print-path --and-exit)
 curl -F "file=@$file" 0x0.st | xclip -selection c
 notify-send "link copied to clipboard"
