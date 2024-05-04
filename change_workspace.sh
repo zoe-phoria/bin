3693cfd17d989fff5073a6056dbabbb28d92ad97
zoe-phoria <mueller-zoe@pm.me>
2024-05-04 13:07:01 +0200

    make script react to $1 to find out if it changes to next or previous workspace

--- change_workspace.sh
+++ change_workspace.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo $1
 current_workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-next_new_workspace=$((current_workspace+1))
-i3-msg workspace number $next_new_workspace
+if [[ $1 = "next" ]]; then
+    next_workspace=$(( current_workspace + 1 ))
+    echo "next"
+elif [[ $1 = "prev" ]]; then
+    next_workspace=$(( current_workspace - 1 ))
+else
+    exit 1
+fi
+i3-msg workspace number $next_workspace
