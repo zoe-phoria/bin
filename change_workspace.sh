35ac6ce53af0528c12fb24d61b2ab6937cae71f0
zoe-phoria <mueller-zoe@pm.me>
2024-05-05 13:54:20 +0200

    remove echos

--- change_workspace.sh
+++ change_workspace.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo $1
 current_workspace=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
 if [[ $1 = "next" ]]; then
     next_workspace=$(( current_workspace + 1 ))
-    echo "next"
 elif [[ $1 = "prev" ]]; then
     next_workspace=$(( current_workspace - 1 ))
 else
