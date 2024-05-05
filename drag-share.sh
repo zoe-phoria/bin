8012718bdf66609d8c57e79d7319476abf9773d9
zoe-phoria <mueller-zoe@pm.me>
2024-05-05 15:53:30 +0200

    check if dragon is available

--- drag-share.sh
+++ drag-share.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-file=$($HOME/bin/dragon --target --print-path --and-exit)
-curl -F "file=@$file" 0x0.st | xclip -selection c
-notify-send "link copied to clipboard"
+if [[ -f $HOME/bin/dragon ]]; then
+    file=$($HOME/bin/dragon --target --print-path --and-exit)
+    curl -F "file=@$file" 0x0.st | xclip -selection c
+    notify-send "link copied to clipboard"
+else
+    exit 1
+fi
