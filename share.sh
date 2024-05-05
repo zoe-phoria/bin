6eff284095b26271f8f3222ad6a27769ae83a788
zoe-phoria <mueller-zoe@pm.me>
2024-05-05 16:05:17 +0200

    check for dependencies

--- share.sh
+++ share.sh
@@ -3,5 +3,13 @@
 # upload file to 0x0.st (alternative: x0.at) with fzf selection
 cwd=$(pwd)
 file=$(find $cwd -type f | fzf)
-curl -F "file=@$file" 0x0.st | xclip -selection c
+
+if [[ $OSTYPE == "linux-gnu" && -f /usr/bin/xclip ]]; then
+    curl -F "file=@$file" 0x0.st | xclip -selection c
+elif [[ $OSTYPE == "darwin"* ]]; then
+    curl -F "file=@$file" 0x0.st | pbcopy
+else
+    curl -F "file=@$file" 0x0.st
+fi
+
 notify-send "link copied to clipboard"
