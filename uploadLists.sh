400d7533d99c10cae07216f9a7ea90130c747388
zoe-phoria <mueller-zoe@pm.me>
2024-06-08 17:02:41 +0200

    check for pacman and add generic exit codes

--- uploadLists.sh
+++ uploadLists.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-if [[ -d "/mnt/files/Software" ]] 
+if [[ ! -f "/usr/bin/pacman" ]]; then
+    printf "\033[01;34m::\033[0m unsupported os\n"
+    exit 1
+fi
+
+if [[ -d "/mnt/files/Software" ]]
 then
     /usr/bin/pacman -Qqn > /mnt/files/Software/pacman/pkglist.$(cat /etc/hostname).txt
     /usr/bin/pacman -Qqetn > /mnt/files/Software/pacman/nodependpkglist.$(cat /etc/hostname).txt 
@@ -9,4 +14,5 @@ then
 else
     printf "\033[01;34m::\033[0m host 'ds718p' (192.168.0.3) is down\n"
     printf "\033[01;34m::\033[0m package lists not updated\n"
+    exit 1
 fi
