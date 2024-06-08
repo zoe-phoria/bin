021a29b59b77890309a10456f182bfe7cec9c27e
zoe-phoria <mueller-zoe@pm.me>
2024-06-08 17:00:13 +0200

    check if Adwaita-for-Steam is (probably) installed

--- adwaitaforsteamupdate.sh
+++ adwaitaforsteamupdate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd $HOME/builds/Adwaita-for-Steam/
-git pull
-chmod +x $HOME/builds/Adwaita-for-Steam/install.py
-/usr/bin/python3 $HOME/builds/Adwaita-for-Steam/install.py -e general/no_rounded_window_corners -e library/hide_whats_new
+if [[ -d $HOME/builds/Adwaita-for-Steam/ ]]; then
+    cd $HOME/builds/Adwaita-for-Steam/
+    git pull
+    chmod +x $HOME/builds/Adwaita-for-Steam/install.py
+    /usr/bin/python3 $HOME/builds/Adwaita-for-Steam/install.py -e general/no_rounded_window_corners -e library/hide_whats_new
+else
+    printf "Adwaita-for-Steam not found in $HOME/builds/\n"
+    exit 1
+fi
