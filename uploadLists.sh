df800102eb4f3b0e4a952d49db3e1125c4eacd88
zoe-phoria <mueller-zoe@pm.me>
2024-06-08 17:17:16 +0200

    use head -n 1 instead of cat in case of line breaks in /etc/hostname file

--- uploadLists.sh
+++ uploadLists.sh
@@ -7,9 +7,9 @@ fi
 
 if [[ -d "/mnt/files/Software" ]]
 then
-    /usr/bin/pacman -Qqn > /mnt/files/Software/pacman/pkglist.$(cat /etc/hostname).txt
-    /usr/bin/pacman -Qqetn > /mnt/files/Software/pacman/nodependpkglist.$(cat /etc/hostname).txt 
-    /usr/bin/pacman -Qqem > /mnt/files/Software/pacman/foreignpkglist.$(cat /etc/hostname).txt
+    /usr/bin/pacman -Qqn > /mnt/files/Software/pacman/pkglist.$(head -n 1 /etc/hostname).txt
+    /usr/bin/pacman -Qqetn > /mnt/files/Software/pacman/nodependpkglist.$(head -n 1 /etc/hostname).txt 
+    /usr/bin/pacman -Qqem > /mnt/files/Software/pacman/foreignpkglist.$(head -n 1 /etc/hostname).txt
     printf "\033[01;34m::\033[0m package lists updated\n"
 else
     printf "\033[01;34m::\033[0m host 'ds718p' (192.168.0.3) is down\n"
