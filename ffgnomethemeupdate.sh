35fe96385f0d0d69a7a4cc98264e6c8c7dde22e0
zoe-phoria <mueller-zoe@pm.me>
2025-01-16 22:46:50 +0100

    add thunderbird-gnome-theme updater

--- ffgnomethemeupdate.sh
+++ ffgnomethemeupdate.sh
@@ -3,23 +3,39 @@
 printf "\ncheck https://github.com/rafaelmardojai/firefox-gnome-theme before proceeding\n"
 read -p "proceed? (y/N) " -n 1 -r
 printf "\n\n"
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     if [[ $OSTYPE == "linux-gnu" ]]; then
         PROFILE=$(ls -l $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')
-        cd $HOME/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/ || { printf "ffgnometheme not installed\n"; exit 1; }
+        cd $HOME/.mozilla/firefox/$PROFILE/chrome/firefox-gnome-theme/ || { printf "ffgnometheme not installed\n"; }
     elif [[ $OSTYPE == "darwin"* ]]; then
         if [[ -z $(ls $HOME/appsupport) ]]; then
             ln -sf $HOME/Library/Application Support/ $HOME/appsupport
         fi
         PROFILE=$(ls -l $HOME/appsupport/Firefox/Profiles/ | grep default-release | awk '{print $NF}')
-        cd $HOME/Library/Application\ Support/Firefox/Profiles/$PROFILE/chrome || { printf "ffgnometheme not installed\n"; exit 1; }
+        cd $HOME/Library/Application\ Support/Firefox/Profiles/$PROFILE/chrome || { printf "ffgnometheme not installed\n"; }
     else
         printf "unsupported OS\n"
-        exit 1
     fi
-    git pull
+    if [[ -n $(git status --porcelain) ]]; then
+        git pull
+    else
+        printf "Already up to date.\n"
+    fi
 else
     printf "aborted\n"
 fi
 printf "\n"
+printf "check https://github.com/rafaelmardojai/thunderbird-gnome-theme before proceeding\n"
+read -p "proceed (y/N) " -n 1 -r
+printf "\n\n"
+if [[ $REPLY =~ ^[Yy]$ && -d $HOME/builds/thunderbird-gnome-theme/ ]]; then
+    cd $HOME/builds/thunderbird-gnome-theme/
+    if [[ -n $(git status --porcelain) ]]; then
+        git pull
+        /bin/bash $HOME/builds/thunderbird-gnome-theme/scripts/auto-install.sh
+    else
+        printf "Already up to date.\n\n"
+    fi
+else
+    printf "thunderbird gnome theme not installed\n\n"
+fi
